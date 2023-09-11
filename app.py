88c39d0ddd225a04412a555f061bf660f138b32d
Yash Sonawane <115136311+YashSonawane31@users.noreply.github.com>
2023-09-11 13:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ AZURE_POSTGRESQL_DATABASE=employee_database
 AZURE_POSTGRESQL_USERNAME=demoadmin
 AZURE_POSTGRESQL_PASSWORD=World&147
 port_id = 5432
+conn = None
+cur = None
 
 def get_db_connection():
     return psycopg2.connect(
@@ -20,6 +22,35 @@ def get_db_connection():
         password=pwd,
         port=port_id
     )
+
+try:
+    conn = psycopg2.connect(
+                host = hostname,
+                dbname = database,
+                user = username,
+                password = pwd,
+                port = port_id)
+
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute('DROP TABLE IF EXISTS employee')
+
+    create_script = ''' CREATE TABLE IF NOT EXISTS employee(
+                            id     int PRIMARY KEY,
+                            name   varchar(40) NOT NULL,
+                            emp_id int) '''
+    
+    cur.execute(create_script)
+    conn.commit()
+
+except Exception as error:
+    print(error)
+
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
 
 @app.route('/', methods=['GET'])
 def index():
