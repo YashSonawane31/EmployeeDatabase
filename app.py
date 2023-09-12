27272e5aabba26ecc29cfc8df7f2e30b16e7ce24
Yash Sonawane <115136311+YashSonawane31@users.noreply.github.com>
2023-09-12 11:00:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 import psycopg2.extras
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 hostname = 'postgres-flask-db.postgres.database.azure.com'
 database = 'postgres'
